0a150705a658116da3f1811f1a50288899a4dbd8
ParkChanWoo <ParkChanWoo@DESKTOP-96Q4II2>
2019-03-04 10:39:38 +0900

    a

---
+++ 01JavaSE/javase_20190304/src/com/test/Main.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//문제) 네자리 년도에 해당하는 숫자를 받고,
+		//현재 년도를 기준으로 나이를 계산해서 출력하는
+		//메소드 작성
+		//매개 변수는 있고, 반환값이 없는 경우
+		//출력 예)
+		//1980 -> 올해(2019) 기준으로 xx살입니다.
+		Sample s = new Sample();
+		s.age(1994);
+	}
+
+}
---
+++ 01JavaSE/javase_20190304/src/com/test/Sample.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Sample {
+	//출력 예)
+	//1980 -> 올해(2019) 기준으로 xx살입니다.
+	int age;
+	int toyear = 2019;
+	
+	void age(int year) {
+		age = (toyear - year) +1;
+		System.out.printf("%d년생은 올해(%d) 기준으로 %d살입니다.",year ,toyear, age);
+	}
+}
