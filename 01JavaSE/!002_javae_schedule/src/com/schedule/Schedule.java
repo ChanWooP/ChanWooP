cfedef255d95f6fcc0d73d08a0e8e679e171ffe1
ParkChanWoo <ParkChanWoo@DESKTOP-96Q4II2>
2019-03-08 14:20:15 +0900

    a

---
+++ 01JavaSE/!002_javae_schedule/src/com/schedule/Schedule.java
@@ -0,0 +1,27 @@
+package com.schedule;
+
+public class Schedule {
+	private String mid;
+	private String date_;
+	private String memo;
+	
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getDate_() {
+		return date_;
+	}
+	public void setDate_(String date_) {
+		this.date_ = date_;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+}
---
+++ 01JavaSE/!002_javae_schedule/src/com/schedule/ScheduleService.java
@@ -0,0 +1,38 @@
+package com.schedule;
+
+import java.util.Scanner;
+
+public class ScheduleService {
+	
+	private ScheduleDAO dao = new ScheduleDAO();
+	
+	public void menu01(Scanner sc) {
+		
+	}
+
+	public void menu02(Scanner sc) {
+		System.out.println("2.일정 출력 및 검색");
+		System.out.println("1.오늘일정 2.");
+	}
+	
+	public void menu02_1(Scanner sc) {
+		
+	}
+	
+	public void menu02_2(Scanner sc) {
+		
+	}
+	
+	public void menu02_3(Scanner sc) {
+		
+	}
+	
+	public void menu02_4(Scanner sc) {
+		
+	}
+	
+	public void menu02_5(Scanner sc) {
+		
+	}
+
+}
